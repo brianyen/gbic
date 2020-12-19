0e6ec662a1bed09f37fa4a927fd6c4ca3f248366
Alex <alexyen1000@gmail.com>
2020-12-19 15:06:03 -0800

    converter-py can extract the json timestamps of mp4

--- code/converter.py
+++ code/converter.py
@@ -2,21 +2,31 @@ import os
 import tempfile
 import time
 import shutil
+import json
 
 def main(url):
     if verify_url(url):
         temp_dir = tempfile.TemporaryDirectory()
         print(temp_dir.name)
-        return convert(url, temp_dir)
+        convert_to_mp4(url, temp_dir)
+        get_iframes_ts()
+        return 
     else:
         return
 
-def convert(url, temp_dir):
-    stream = os.popen('../.././youtube-dl -o "' + temp_dir.name + '/vid.mp4" -f mp4 ' + url)
+def convert_to_mp4(url, temp_dir):
+    #downloads video mp4 for ffmpeg
+    stream = os.popen('../.././youtube-dlc -o "' + temp_dir.name + '/vid.mp4" --write-auto-sub --sub-format json3 -f mp4 ' + url)
     output = stream.read()
-    time.sleep(500)
+    print(output)
     return output
 
+def get_iframes_ts():
+    stream = os.popen('ffprobe -show_frames -of json -f lavfi "movie=test-iframes/radix.mp4,select=gt(scene\\,0.1)"')
+    output = stream.read()
+    json = output[output.index("{"):]
+    print('----------')
+    print(json)
 
 def verify_url(url):
     return True
