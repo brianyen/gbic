eb4ff8a6fe8653c4fc4984c311e47d90cb5315bf
Alex <alexyen1000@gmail.com>
2020-12-20 21:55:12 -0800

    frames are selected and stored in temp_dir based on timestamps of iframes

--- code/converter.py
+++ code/converter.py
@@ -3,30 +3,47 @@ import tempfile
 import time
 import shutil
 import json
+import math
 
 def main(url):
     if verify_url(url):
         temp_dir = tempfile.TemporaryDirectory()
         print(temp_dir.name)
         convert_to_mp4(url, temp_dir)
-        timestamps = get_iframes_ts()
+        timestamps = get_iframes_ts(temp_dir)
+        frames = get_iframes(temp_dir, timestamps)
+        print(frames)
         subtitles = get_subtitles_with_ts(temp_dir)
         return 
     else:
         return
 
 def convert_to_mp4(url, temp_dir):
-    #downloads video mp4 for ffmpeg
+    #downloads video mp4
     stream = os.popen('../.././youtube-dlc -o "' + temp_dir.name + '/vid.mp4" --write-auto-sub --sub-format json3 -f mp4 ' + url)
     output = stream.read()
     return output
 
-def get_iframes_ts():
+def get_iframes(temp_dir, timestamps):
+    min_frame_diff = 5
+    last_ts = -math.inf
+    frames = []
+    for ts in timestamps:
+        if ts - last_ts < min_frame_diff:
+            continue
+        stream = os.popen('ffmpeg -ss ' + str(ts) + ' -i ' + temp_dir.name + '/vid.mp4 -c:v png -frames:v 1 ' + temp_dir.name + '/frame-' + str(ts) + '.png')
+        output = stream.read()
+        frames.append('frame-' + str(ts) + '.png')
+        last_ts = ts
+    return frames
+
+def get_iframes_ts(temp_dir):
     stream = os.popen('ffprobe -show_frames -of json -f lavfi "movie=test-iframes/radix.mp4,select=gt(scene\\,0.1)"')
     output = stream.read()
     metadata = output[output.index("{"):]
     metadata_dict = json.loads(metadata)
     timestamps = []
+    timestamps.append(0.0)
 
     for frame in metadata_dict['frames']:
         timestamps.append(float(frame.get('best_effort_timestamp_time')))
@@ -34,11 +51,11 @@ def get_iframes_ts():
 
 def get_subtitles_with_ts(temp_dir):
     with open(temp_dir.name + '/vid.en.json3', 'r') as f:
-        caption_json = f.read()
-    caption_dict = json.loads(caption_json)
+        subtitle_json = f.read()
+    subtitle_dict = json.loads(subtitle_json)
 
-    captions_with_ts = []
-    for event in caption_dict['events']:
+    subtitles_with_ts = []
+    for event in subtitle_dict['events']:
         sentence = ""
         if "segs" in event:
             for word in event['segs']:
@@ -48,9 +65,8 @@ def get_subtitles_with_ts(temp_dir):
             "sentence": sentence
         }
         if line['sentence'] != '\n' and len(line['sentence']) > 0:
-            captions_with_ts.append(line)
-    print(captions_with_ts)
-    return captions_with_ts
+            subtitles_with_ts.append(line)
+    return subtitles_with_ts
 
 def verify_url(url):
     return True
