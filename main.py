ba0c71bed1102ca8cbd9c9e736df0136bf264c98
brianyen <53711755+brianyen@users.noreply.github.com>
2020-12-30 17:13:11 -0800

    connect to gcloud

---
+++ main.py
@@ -0,0 +1,30 @@
+from jinja2 import Template
+import json
+
+def render(url):
+    with open('data.json') as f:
+        data = f.read()
+
+    slides = json.loads(data)
+
+    with open('html.tmpl') as q:
+        html = q.read()
+
+    t = Template(html)
+
+    return t.render(slides=slides, url=url)
+ 
+def convert_url(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    if request.args and 'url' in request.args:
+        print("hi")
+        return render(request.args.get('url'))
+    else:
+        return "figure something out"
