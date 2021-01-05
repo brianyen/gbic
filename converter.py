d811714e3cad3199ead46e024c4d224ed539deef
Alex <alexyen1000@gmail.com>
2021-01-04 21:19:27 -0800

    adds google cloud implicit test

--- converter.py
+++ converter.py
@@ -12,6 +12,8 @@ download_url = sys.argv[1]
 out_dir = sys.argv[2]
 ytdl_prefix = sys.argv[3]
 
+implicit()
+
 #long video
 #download_url = "https://www.youtube.com/watch?v=R44tKAPpKOM"
 
@@ -238,6 +240,17 @@ def upload_to_bucket(blob_name, path_to_file, bucket_name):
 
     #returns a public url
     return blob.public_url
+
+def implicit():
+    from google.cloud import storage
+
+    # If you don't specify credentials when constructing the client, the
+    # client library will look for credentials in the environment.
+    storage_client = storage.Client()
+
+    # Make an authenticated API request
+    buckets = list(storage_client.list_buckets())
+    print(buckets)
 
 ##############################################################
 
