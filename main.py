8152b7e522fb638dd9fed77cb017e724399e6e98
Steve Yen <steve.yen@gmail.com>
2021-01-16 22:12:17 -0800

    cookies_refresh() added to main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from jinja2 import Template
 import json
 import os
 import urllib
@@ -7,6 +6,7 @@ import converter
 import base64
 import re
 import flask
+
 from google.cloud import pubsub_v1
 
 def convert_url_sub(event, context):
@@ -21,6 +21,8 @@ def convert_url_sub(event, context):
         return converter.main(data_dict.get("url"), data_dict.get("out_dir"))
     else:
         return "nothing3"
+
+# -------------------------------------------------------
 
 def convert_url(request):
     url = str(request.args.get('url'))
@@ -43,3 +45,29 @@ def convert_url(request):
     print(f"Published message to {topic_path}.")
 
     return flask.redirect('https://www.tubeslides.net/view.html?out_dir=' + out_dir)
+
+# -------------------------------------------------------
+
+def cookies_refresh(event, context):
+    import os, http.cookiejar, urllib.request
+
+    cj = http.cookiejar.MozillaCookieJar()
+
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+
+    r = opener.open("https://www.youtube.com")
+
+    print("cj", cj)
+
+    cj.save("/tmp/cookies.txt")
+
+    print("cj saved")
+
+    gcs_client = storage.Client(project='gbic')
+    bucket = gcs_client.get_bucket('www.tubeslides.net')
+    blob = bucket.blob('cookies.txt')
+
+    print("cookies.txt uploading")
+    blob.upload_from_filename('/tmp/cookies.txt')
+
+    print("cookies.txt uploaded")
