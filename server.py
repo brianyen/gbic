5d877da2167d6aa447c0be1ca4924721b6d89571
brianyen <53711755+brianyen@users.noreply.github.com>
2021-11-13 17:57:27 -0800

    fixed post

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.send_from_directory('.', 'index.html')
+    return flask.send_file('index.html')
 
 @app.route('/view.html')
 def view():
-    return flask.send_from_directory('.', 'view.html')  
+    return flask.send_file('view.html')
     
-@app.route('/go')
+@app.route('/go', methods = ['POST'])
 def go():
     out_dir = "ha" 
     return flask.redirect('/view.html?out_dir=' + out_dir)
