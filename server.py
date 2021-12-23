838419e7ba6fb64bc6508423c51cb47d9a51d8c5
brianyen <53711755+brianyen@users.noreply.github.com>
2021-12-22 20:07:28 -0800

    progress bar work

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import flask
 from flask import request, send_from_directory
 
-
 import re
 import urllib
 import subprocess
+
+progress = {}
 
 app = flask.Flask(__name__)
 
@@ -26,11 +27,19 @@ def go():
     out_dir = out_dir.replace("%", "-")
 
     subprocess.Popen(['py', 'converter.py', url, out_dir])
+    
+    # spawn a child thread, which reads the stdout from converter.py
+    # and then updates the global progress dict, like...
+    #  progress[out_dir] = 130
 
     return flask.redirect('/view.html?out_dir=' + out_dir)
 
 @app.route('/v/<path:p>')
 def v(p):
     return send_from_directory('temp', p)
+
+@app.route('/progress/<string:v>')
+def progress(v):
+    return progress[v]
 
 app.run(host='0.0.0.0', port=81, debug=True)
